477f3a759bbe1b727efcd05c7953c737cae4ef2f
cryorepository <cryorepository@protonmail.com>
2025-10-02 22:00:08 +1000

    Add page metadata /ivx

--- src/app/ivx/page.tsx
+++ src/app/ivx/page.tsx
@@ -8,6 +8,52 @@ import { JBChainId } from "juice-sdk-react";
 import { notFound } from "next/navigation";
 import MainIvxLayout from "./components/Main";
 
+import { headers } from "next/headers";
+import type { Metadata } from "next";
+import { metadata } from "@/lib/metadata";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const headersList = await headers();
+  const host = headersList.get("host");
+  const proto = headersList.get("x-forwarded-proto") || "http";
+  const origin = `${proto}://${host}`;
+
+  const fullPath = "/";
+  const url = new URL(fullPath, origin);
+
+  const imgUrl = `${origin}/assets/img/branding/seo_banner.png`;
+
+  return {
+    title: "IVX Token | Inevitable Protocol",
+    description: metadata.description,
+    alternates: {
+      canonical: url,
+    },
+    openGraph: {
+      title: "IVX Token | Inevitable Protocol",
+      description: metadata.description,
+      siteName: metadata.siteName,
+      images: [
+        {
+          url: imgUrl,
+          width: 700,
+          height: 370,
+          alt: "Inevitable preview image",
+        },
+      ],
+      url: url,
+      type: "website",
+    },
+    twitter: {
+      title: "IVX Token | Inevitable Protocol",
+      description: metadata.description,
+      card: "summary_large_image",
+      images: [imgUrl],
+    },
+    manifest: metadata.manifest,
+  };
+};
+
 async function fetchIvxData() {
   try {
     const [tokenRes, treasuryRes] = await Promise.all([
