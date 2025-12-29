664b53ba363280dd2a232eeae636508a6e6ebd05
cryorepository <cryorepository@protonmail.com>
2025-12-29 21:08:01 +1100

    update not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -47,53 +47,56 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default function NotFound() {
   return (
-    <section className="ctWrapper pt-12 h-screen bg-[url('https://cdn.inevitable.science/static/img/layout/footer.webp')] bg-cover bg-screen relative mask-[linear-gradient(to_bottom,black_30%,black_90%,transparent_100%)]">
-      <div className="flex flex-col">
-        <div className="flex uppercase flex-col md:flex-row">
-          <h1 className="font-optima text-5xl sm:text-6xl italic">
-            Page
-          </h1>
-          <div className="not-italic md:pt-[3.75rem] flex flex-col inline-block">
+    <div className="bg-[url('https://cdn.inevitable.science/static/img/layout/footer.webp')] bg-cover bg-screen relative mask-[linear-gradient(to_bottom,black_30%,black_90%,transparent_100%)]">
+      <section className="ctWrapper pt-12 h-screen">
+        <div className="flex flex-col">
+          <div className="flex uppercase flex-col md:flex-row">
             <h1 className="font-optima text-5xl sm:text-6xl">
-              Not Found
+              Page
             </h1>
+            <div className="not-italic md:pt-[3.75rem] flex flex-col inline-block">
+              <h1 className="font-optima text-5xl sm:text-6xl">
+                Not Found
+              </h1>
 
-            <p className="my-8 max-w-[350px] text-wrap">
-              Looks like this page wandered off... Try heading back to the homepage to get back on track.
-            </p>
+              <p className="my-8 max-w-[350px] text-wrap">
+                Looks like this page wandered off... Try heading back to the homepage to get back on track.
+              </p>
 
-            <Button variant={"accent"}>
-              <Link href="/" className="flex items-center gap-4">
-                Return To Homepage
-                <ArrowRight height={18} width={18} />
-              </Link>
-            </Button>
+              <Button variant={"accent"}>
+                <Link href="/" className="flex items-center gap-4">
+                  Return To Homepage
+                  <ArrowRight height={18} width={18} />
+                </Link>
+              </Button>
+            </div>
+          </div>
+
+          <div className="absolute bottom-0 left-[60vw]">
+            <img // use img as Image does not preserve image quality
+              src="https://cdn.inevitable.science/static/img/hero.webp"
+              alt="Hero Image"
+              className="notFoundHero w-[230px] sm:block hidden"
+            />
           </div>
         </div>
+        <style>{`
 
-        <div className="absolute bottom-0 left-[60vw]">
-          <img // use img as Image does not preserve image quality
-            src="https://cdn.inevitable.science/static/img/hero.webp"
-            alt="Hero Image"
-            className="notFoundHero w-[230px] sm:block hidden"
-          />
-        </div>
-      </div>
-      <style>{`
-      @media (max-height:630px) {
-        .notFoundHero {
+        @media (max-height:630px) {
+          .notFoundHero {
+            display: none;
+          }
+        }
+
+        nav{
           display: none;
         }
-      }
 
-      nav{
-        display: none;
-      }
-
-      footer{
-        display: none !important;
-      }
-      `}</style>
-    </section>
+        footer{
+          display: none !important;
+        }
+        `}</style>
+      </section>
+    </div>
   );
 }
