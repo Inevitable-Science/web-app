93a32e8f7f87e4e8d230bca60f12e0af94f370b0
cryorepository <cryorepository@protonmail.com>
2025-12-30 17:54:08 +1100

    update activity feed pagination

--- src/app/rev/[...slug]/components/sections/activity/ActivityFeed.tsx
+++ src/app/rev/[...slug]/components/sections/activity/ActivityFeed.tsx
@@ -1,33 +1,27 @@
 import EtherscanLink from "@/components/EtherscanLink";
 import {
   ActivityEventsDocument,
+  ActivityEventsQuery,
   CashOutTokensEvent,
   PayEvent,
-  ProjectDocument,
 } from "@/generated/graphql";
 import { formatTokenSymbol } from "@/lib/utils";
 import { formatDistance } from "date-fns";
 import { Ether, JB_CHAINS, JBProjectToken } from "juice-sdk-core";
 import {
   JBChainId,
-  useJBChainId,
-  useJBContractContext,
   useJBTokenContext,
   useBendystrawQuery,
   useJBProjectMetadataContext,
 } from "juice-sdk-react";
 import { useState, useEffect, useMemo } from "react";
-import { Address, Chain, formatEther } from "viem";
+import { Address } from "viem";
 
 import { ChevronLeft, ChevronRight, Loader2 } from "lucide-react";
-import StaticVolumeChart, {
-  ProjectTimelineRange,
-  ProjectTimelineView,
-} from "../../ActivityGraph";
-import { useVolumeData } from "@/hooks/useVolumeData";
-//import { useProjectContext } from "../../../ProjectDataContext";
+import StaticVolumeChart from "../../ActivityGraph";
 import { EthereumAddress } from "@/components/EthereumAddress";
 import { Button } from "@/components/ui/button";
+import { useProjectDataStore } from "../../../ProjectDataContext";
 
 function PayActivityItem(
   payEvent: Pick<
@@ -152,54 +146,20 @@ function RedeemActivityItem(
 }
 
 export function ActivityFeed() {
-  const { projectId, version } = useJBContractContext();
-  const chainId = useJBChainId();
-  const [isOpen, setIsOpen] = useState(true);
+  const project = useProjectDataStore(state => state.project);
+  const suckerGroupId = project.suckerGroupId;
 
-  // --- 1. Manage the chart's state here in the parent ---
-  const [view, setView] = useState<ProjectTimelineView>("volume");
-  const [range, setRange] = useState<ProjectTimelineRange>(30); // Default to 30 days
   const [indexNum, setIndexNum] = useState(0);
+  const [currentData, setCurrentData] = useState<ActivityEventsQuery | null>(null);
 
   const activityEventsLimit = 45;
   const activityEventsOffset = indexNum * activityEventsLimit;
   const page = indexNum + 1;
 
-  const { data: project } = useBendystrawQuery(ProjectDocument, {
-    chainId: Number(chainId),
-    projectId: Number(projectId),
-    version: Number(version),
-    skip: !chainId || !projectId || !version,
-  });
-  const suckerGroupId = project?.project?.suckerGroupId;
-
-  // --- 2. Make the data fetching dynamic based on the `range` state ---
-  const [loadTimestamp] = useState(() => Math.floor(Date.now() / 1000));
-  const startTimestamp = useMemo(() => {
-    // Calculate the start timestamp based on the selected range
-    return loadTimestamp - range * 24 * 60 * 60;
-  }, [loadTimestamp, range]);
-
-  const { dailyTotals, isLoading: isChartLoading } = useVolumeData({
-    suckerGroupId,
-    startTimestamp,
-    endTimestamp: loadTimestamp,
-  });
-
-  // --- 3. Format the fetched data for the chart ---
-  const formattedChartData = useMemo(() => {
-    return dailyTotals.map((day) => ({
-      timestamp: Math.floor(day.date.getTime() / 1000),
-      volume: Number(formatEther(day.volume)),
-      balance: 0,
-      trendingScore: 0,
-    }));
-  }, [dailyTotals]);
-
   const {
     data: activityEvents,
     isLoading,
-    isFetching,
+    isFetching
   } = useBendystrawQuery(
     ActivityEventsDocument,
     {
@@ -220,7 +180,13 @@ export function ActivityFeed() {
     }
   );
 
-  const totalActivityEvents = activityEvents?.activityEvents.totalCount ?? 0;
+  useEffect(() => {
+    if (activityEvents) {
+      setCurrentData(activityEvents);
+    }
+  }, [activityEvents, isLoading]);
+
+  const totalActivityEvents = currentData?.activityEvents.totalCount ?? 0;
   const totalPages = useMemo(() => {
     return totalActivityEvents ? Math.ceil(totalActivityEvents / activityEventsLimit) : 0;
   }, [totalActivityEvents]);
@@ -231,97 +197,93 @@ export function ActivityFeed() {
         <StaticVolumeChart suckerGroupId={suckerGroupId} />
       </section>
 
-      {isOpen && (
-        <div className="flex flex-col gap-1">
-          {isLoading ? (
-            <div className="my-[15vh] flex w-full justify-center">
-              <Loader2 className="h-8 w-8 animate-spin" />
-            </div>
-          ) : activityEvents?.activityEvents.items &&
-            activityEvents.activityEvents.items.length > 0 ? (
-              <>
-              <div className="min-h-[150px]">
-                {activityEvents.activityEvents.items?.map((event) => {
-                  if (event?.payEvent) {
-                    return (
-                      <PayActivityItem
-                        key={event.id}
-                        chainId={event.chainId as JBChainId}
-                        {...event.payEvent}
-                      />
-                    );
-                  }
-                  if (event?.cashOutTokensEvent) {
-                    return (
-                      <RedeemActivityItem
-                        key={event.id}
-                        chainId={event.chainId as JBChainId}
-                        {...event.cashOutTokensEvent}
-                      />
-                    );
-                  }
-
-                  return null;
-                })}
-                </div>
-                
-                <div className="mt-6 flex flex-col items-center gap-2">
-                  <p className="text-sm font-light text-muted-foreground">
-                    Page {page} out of {totalPages}
-                  </p>
-                  <div className="flex items-center gap-2">
-                    <Button
-                      variant="outline"
-                      size="icon"
-                      onClick={() => setIndexNum((prev) => Math.max(0, prev - 1))}
-                      disabled={page === 1}
-                    >
-                      <ChevronLeft className="h-4 w-4" />
-                    </Button>
-
-                    {Array.from({ length: 3 }, (_, i) => {
-                      const start = Math.max(
-                        1,
-                        Math.min(page - 1, totalPages - 2)
-                      );
-
-                      const pageNum = start + i;
-                      if (pageNum > totalPages) return null;
-
-                      return (
-                        <Button
-                          key={pageNum}
-                          variant={pageNum === page ? "default" : "outline"}
-                          className={`${pageNum === page ? "border-color border" : ""} font-light`}
-                          onClick={() => setIndexNum(pageNum - 1)} // indexNum is page - 1
-                        >
-                          {pageNum}
-                        </Button>
-                      );
-                    })}
-
-                    <Button
-                      variant="outline"
-                      size="icon"
-                      onClick={() =>
-                        setIndexNum((prev) => Math.min(totalPages + 1, prev + 1))
-                      }
-                      disabled={page === totalPages}
-                    >
-                      <ChevronRight className="h-4 w-4" />
-                    </Button>
-                  </div>
-                  <p className="text-sm font-light text-muted-foreground">
-                    Showing {Math.min(activityEventsLimit, totalActivityEvents)} items out of {totalActivityEvents}
-                  </p>
-                </div>
-              </>
-          ) : (
-            <span className="text-md text-muted-foreground">
-              No activity yet.
-            </span>
-          )}
+      {!currentData ? (
+        <div className="my-[15vh] flex w-full justify-center">
+          <Loader2 className="h-8 w-8 animate-spin" />
         </div>
+      ) : currentData?.activityEvents.items && currentData.activityEvents.items.length > 0 ? (
+        <>
+        <div className="min-h-[150px]">
+          {currentData.activityEvents.items?.map((event) => {
+            if (event?.payEvent) {
+              return (
+                <PayActivityItem
+                  key={event.id}
+                  chainId={event.chainId as JBChainId}
+                  {...event.payEvent}
+                />
+              );
+            }
+            if (event?.cashOutTokensEvent) {
+              return (
+                <RedeemActivityItem
+                  key={event.id}
+                  chainId={event.chainId as JBChainId}
+                  {...event.cashOutTokensEvent}
+                />
+              );
+            }
+
+            return null;
+          })}
+          </div>
+            
+          <div className="mt-6 flex flex-col items-center gap-2">
+            <p className="text-sm font-light text-muted-foreground">
+              Page {page} out of {totalPages}
+            </p>
+            <div className="flex items-center gap-2">
+              <Button
+                variant="outline"
+                size="icon"
+                onClick={() => setIndexNum((prev) => Math.max(0, prev - 1))}
+                disabled={page === 1 || isFetching}
+              >
+                <ChevronLeft className="h-4 w-4" />
+              </Button>
+
+              {Array.from({ length: 3 }, (_, i) => {
+                const start = Math.max(
+                  1,
+                  Math.min(page - 1, totalPages - 2)
+                );
+
+                const pageNum = start + i;
+                if (pageNum > totalPages) return null;
+
+                return (
+                  <Button
+                    key={pageNum}
+                    variant={pageNum === page ? "default" : "outline"}
+                    className={`${pageNum === page ? "border-color border" : ""} font-light`}
+                    onClick={() => setIndexNum(pageNum - 1)} // indexNum is page - 1
+                    disabled={isFetching}
+                  >
+                    {pageNum}
+                  </Button>
+                );
+              })}
+
+              <Button
+                variant="outline"
+                size="icon"
+                onClick={() =>
+                  setIndexNum((prev) => Math.min(totalPages + 1, prev + 1))
+                }
+                disabled={page === totalPages || isFetching}
+              >
+                <ChevronRight className="h-4 w-4" />
+              </Button>
+            </div>
+            <p className="text-sm font-light text-muted-foreground">
+              Showing {Math.min(activityEventsLimit, totalActivityEvents)} items out of {totalActivityEvents}
+            </p>
+          </div>
+        </>
+      ) : (
+        <span className="text-md text-muted-foreground">
+          No activity yet.
+        </span>
       )}
     </>
   );
