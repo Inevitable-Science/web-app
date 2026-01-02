7e1cdaeea817d350ec151d56f46fe7899856dd4e
cryorepository <cryorepository@protonmail.com>
2026-01-02 23:30:49 +1100

    update footer to add /ecosystem page and whitepaper link

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -18,6 +18,9 @@ export default function Footer() {
           <Link href="/" aria-label="Auctions" className="hover:underline">
             Projects
           </Link>
+          <Link href="/ecosystem" aria-label="Auctions" className="hover:underline">
+            Ecosystem
+          </Link>
           <Link href="/vision" aria-label="Vision" className="hover:underline">
             Vision
           </Link>
@@ -38,47 +41,58 @@ export default function Footer() {
       {/* BOTTOM SECTION */}
       <div className="mb-8 flex w-full flex-col-reverse items-center justify-between gap-4 text-center text-sm md:mb-2 md:flex-row md:gap-0">
         <p>All rights reserved Inevitable 2025</p>
-        <div className="flex items-center gap-4">
-          <a
-            href="https://x.com/inevitablesci"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="https://cdn.inevitable.science/static/img/logo/socials/x.svg"
-              alt="X Logo"
-              height={16}
-              width={16}
-            />
-          </a>
+        <div className="flex items-center gap-4 flex-col sm:flex-row">
+          <div className="flex items-center gap-4">
+            <a
+              href="https://x.com/inevitablesci"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="https://cdn.inevitable.science/static/img/logo/socials/x.svg"
+                alt="X Logo"
+                height={16}
+                width={16}
+              />
+            </a>
 
-          <a
-            href="https://discord.gg/inevitable"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="https://cdn.inevitable.science/static/img/logo/socials/discord.svg"
-              alt="Discord Logo"
-              height={20}
-              width={20}
-            />
-          </a>
-
-          <Link
-            href="/legal/terms"
-            aria-label="Terms and Conditions"
-            className="hover:underline"
-          >
-            Terms
-          </Link>
-          <Link
-            href="/legal/privacy"
-            aria-label="Privacy Policy"
-            className="hover:underline"
-          >
-            Privacy
-          </Link>
+            <a
+              href="https://discord.gg/inevitable"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="https://cdn.inevitable.science/static/img/logo/socials/discord.svg"
+                alt="Discord Logo"
+                height={20}
+                width={20}
+              />
+            </a>
+          </div>
+          
+          <div className="flex items-center gap-4">
+            <Link
+              href="/legal/privacy"
+              aria-label="Privacy Policy"
+              className="hover:underline"
+            >
+              Whitepaper
+            </Link>
+            <Link
+              href="/legal/terms"
+              aria-label="Terms and Conditions"
+              className="hover:underline"
+            >
+              Terms
+            </Link>
+            <Link
+              href="/legal/privacy"
+              aria-label="Privacy Policy"
+              className="hover:underline"
+            >
+              Privacy
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
